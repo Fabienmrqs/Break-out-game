7d659cb73fe29e7bef2f6b3de7f3ff0f4c8101b5
Marques Fabien <fabienmrqs@gmail.com>
2019-05-02 17:35:48 +0200

    add game over

--- game.js
+++ game.js
@@ -1,11 +1,38 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var leftPressed = false;
+var rightPressed = false;
+
 var x = canvas.width/2
 var y = canvas.height-30;
 var ballRadius = 10;
 var dx = 2;
 var dy = -2;
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -15,9 +42,18 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     x += dx;
     y += dy;
 
@@ -25,8 +61,24 @@ function draw() {
         dx = -dx;
     }
 
-    if(y + dy > canvas.height-ballRadius || y + dy < 0) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("XDDDD");
+            document.location.reload();
+        }
+    }
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 5;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 5;
     }
 }
 
